1e95a8efda641507b61f9ce504e22720fe5f0ecc
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-04-25 13:03:45 +0200

    packaging

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name='ospi',
-    version='1.0',
     description='This library contains scripts for working with OpenSim files and pinocchio software.',
+    version='1.0.1',
+    packages=['ospi'],
     keywords='biomechanics OpenSim pinocchio',
-    url='https://github.com/GaloMALDONADO/ospi/',
-    license='GNU General Public License v3.0',
-    
+    url='https://github.com/gepetto/ospi/',
     author='Galo MALDONADO',
-    author_email='galo_xav@hotmail.com',
-
-    packages=find_packages(exclude=['models','doc','build']),
-    
+    author_email='galo.maldonado@laas.fr',
+    license='LGPL',
+    install_requires=['numpy'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ],
-    
-    install_requires=['numpy'],
 )
